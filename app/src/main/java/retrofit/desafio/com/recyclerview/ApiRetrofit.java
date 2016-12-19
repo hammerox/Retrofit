d774d763c7f64d1ab90d8f790ced2cb3d1453bac
Mauricio Custodio <mauricio_rc2@hotmail.com>
2016-12-19 17:15:38 -0200

    Two different calls

--- app/src/main/java/retrofit/desafio/com/recyclerview/ApiRetrofit.java
+++ app/src/main/java/retrofit/desafio/com/recyclerview/ApiRetrofit.java
@@ -4,6 +4,7 @@ import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Thiago on 19/12/2016.
@@ -13,6 +14,7 @@ public interface ApiRetrofit {
 
     @GET("data/2.5/history/city?id=2885679&type=hour")
     Call<RequestBody> leRetrofit();
+
     @GET("data/2.5/history/city")
-    Call<RequestBody> leRetrofit();
+    Call<RequestBody> getCity(@Query("id") String id, @Query("type") String type);
 }
